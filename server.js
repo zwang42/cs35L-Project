34d04d160e940a7251c3574d9d9a8ed0e4e874f6
Donald Chung <dchung679@gmail.com>
2021-11-08 00:26:01 -0800

    didn't delete some part

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const app = express();
+const db = require('./db.js');
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
 });
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-      // Connect the client to the server
-      await client.connect();
-      // Establish and verify connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Connected successfully to server");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
 
 app.listen(process.env.PORT || '8080', () => console.log('Server is running on port 8080'))
