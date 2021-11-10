8008a04cb7699bff9ec5675cc41435d6011419f6
Donald Chung <dchung679@gmail.com>
2021-11-10 01:31:23 -0800

    firebase set up and auth

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import { Container } from "react-bootstrap";
 import Post from './Post'
+import { AuthProvider } from "./contexts/AuthContext.js";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Login from "./pages/Login.js";
+import Homepage from "./pages/Homepage.js";
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username:"testUser", 
-      caption:"This is caption. YEP. That it is. A caption.",
-      image: "https://cdna.artstation.com/p/assets/images/images/018/776/422/large/coax-22.jpg?1560694257",
-    },
-  ]);
+  const auth = firebase.auth();
+
+  const [user] = useAuthState(auth)
+  console.log(user); 
   return (
-    <div className="app">
-        <div className = "app__header">
-          <img
-            className = "app__headerImage"
-            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
-            alt=""
-          />
-        </div>
+    <Container style = {{minHeight: "100vh", minWidth: "100vw", margin: 0, padding: 0, border: 0}} > 
+      <div className = "d-flex align-items-center justify-content-center"
+      style = {{ minHeight: "80vh" }}
+      >
 
-        {
-          posts.map(post => (
-            <Post username={post.username} caption={post.caption} image={post.image} />
-          ))
-        }
-
-
-        <Post username="testUser" caption="This is caption. YEP. That it is. A caption." image="https://cdna.artstation.com/p/assets/images/images/018/776/422/large/coax-22.jpg?1560694257"/>
-        <Post username="bananas" caption="bananamananana" image="https://th-thumbnailer.cdn-si-edu.com/4Nq8HbTKgX6djk07DqHqRsRuFq0=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg"/>
-        <Post username="apple" caption="appleplepeleple" image="https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png"/>
-    </div>
+      <Router>
+          <Switch>
+            <Route path = "/login" component = {Login} />
+            <Route path = "/" component = {Homepage} />
+          </Switch>
+      </Router>
+      </div>
+	</Container>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,56 @@
+import React, { createContext, useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { AuthContext } from "../contexts/AuthContext.js";
+import { Link, useHistory } from "react-router-dom";
+
+export default function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { login } = createContext(AuthContext);
+    const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+	const history = useHistory();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            setError("");
+            setLoading(true);
+
+			await login(emailRef.current.value, passwordRef.current.value);
+			history.push("/profile");
+        } catch {
+            setError("Failed to sign in!");
+        }
+
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            <Card style = {{ width: "400px" }}>
+                <Card.Body>
+                    <h2 className = "text-center mb-4">Log In</h2>
+                    {error && <Alert variant = "danger">{error}</Alert>}
+                    <Form onSubmit = {handleSubmit}>
+                        <Form.Group id = "email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type = "email" ref = {emailRef} required></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group id = "password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type = "password" ref = {passwordRef} required></Form.Control>
+                        </Form.Group>
+
+                        <Button disabled = {loading} className = "w-100" type = "submit">Log In</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className = "w-100 text-center mt-2">
+                Need an account? <Link to ="signup">Sign Up</Link>
+            </div>
+        </div>
+    )
+}
