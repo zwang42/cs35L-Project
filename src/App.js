76ba93d477be514b63e1606fe29be0930d047450
Chris Wang <ziyuan28@gmail.com>
2021-11-18 21:18:01 -0800

    Connected firebase to posts, automatically load posts from firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post'
+import {db } from './firebase';
 
 function App() {
-  const [posts, setPosts] = useState([
+ /* const [posts, setPosts] = useState([
     {
       username:"testUser", 
       caption:"This is caption. YEP. That it is. A caption.",
       image: "https://cdna.artstation.com/p/assets/images/images/018/776/422/large/coax-22.jpg?1560694257",
     },
-  ]);
+  ]);*/
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, [posts])
+
   return (
     <div className="app">
         <div className = "app__header">
@@ -22,15 +33,11 @@ function App() {
         </div>
 
         {
-          posts.map(post => (
-            <Post username={post.username} caption={post.caption} image={post.image} />
+          posts.map(({id, post}) => (
+            <Post key={id} username={post.username} caption={post.caption} image={post.image} />
           ))
         }
 
-
-        <Post username="testUser" caption="This is caption. YEP. That it is. A caption." image="https://cdna.artstation.com/p/assets/images/images/018/776/422/large/coax-22.jpg?1560694257"/>
-        <Post username="bananas" caption="bananamananana" image="https://th-thumbnailer.cdn-si-edu.com/4Nq8HbTKgX6djk07DqHqRsRuFq0=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg"/>
-        <Post username="apple" caption="appleplepeleple" image="https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png"/>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase"
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyD5bLDqLXNPsK9e28XYsXe0tfuj-3VAtGY",
+  authDomain: "lproject-1bc54.firebaseapp.com",
+  projectId: "lproject-1bc54",
+  storageBucket: "lproject-1bc54.appspot.com",
+  messagingSenderId: "274092324594",
+  appId: "1:274092324594:web:6612fa04412bf64b71426b",
+  measurementId: "G-7J240Y60ZS"
+});
+
+const db = app.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+export {db, auth, storage};
