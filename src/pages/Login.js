3cb854c3350f9bd4397e31122314c8ec0cee130a
Tyler Xu <tylerxu@Tylers-MacBook-Pro.local>
2021-11-26 17:44:39 -0800

    frontend design for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,31 +27,30 @@ export default function Login() {
 
         setLoading(false);
     }
-
     return (
         <div>
-            <Card style = {{ width: "400px" }}>
-                <Card.Body>
-                    <h2 className = "text-center mb-4">Log In</h2>
-                    {error && <Alert variant = "danger">{error}</Alert>}
-                    <Form onSubmit = {handleSubmit}>
-                        <Form.Group id = "email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type = "email" ref = {emailRef} required></Form.Control>
-                        </Form.Group>
-
-                        <Form.Group id = "password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type = "password" ref = {passwordRef} required></Form.Control>
-                        </Form.Group>
-
-                        <Button disabled = {loading} className = "w-100" type = "submit">Log In</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className = "w-100 text-center mt-2">
-                Need an account? <Link to ="signup">Sign Up</Link>
+            <div class="background">
+                <div class="topshape"></div>
+                <div class="topshape"></div>
             </div>
+            {error && <Alert variant = "danger">{error}</Alert>}
+            <Form onSubmit = {handleSubmit}>
+                <h1>FitnessGram Login</h1>
+                <Form.Group id = "email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type = "email" ref = {emailRef} required></Form.Control>
+                </Form.Group>
+
+                <Form.Group id = "password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type = "password" ref = {passwordRef} required></Form.Control>
+                </Form.Group>
+
+                <Button disabled = {loading} className = "w-100" type = "submit">Log In</Button>
+                <div className = "alt">
+                    Need an account? <Link to ="signup">Sign Up</Link>
+                </div>
+            </Form>
         </div>
     )
 }
